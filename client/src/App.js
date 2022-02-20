c601b31cfe59ac7e1634b3b9b0a5c42cff707fd8
HariKiran <harikiran.kante@gmail.com>
2022-02-21 01:08:42 +0530

    added login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Routes/Home";
-import Header from "./Components/Header";
+import ErrorPage from "./Routes/ErrorPage";
+import LoginPage from "./Routes/LoginPage";
 function App() {
   return (
     <div className="App">
       <Router>
-        <Header />
+        {/* <Header /> */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="*" element={<ErrorPage />} />
         </Routes>
       </Router>
     </div>
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -2,24 +2,23 @@ import React from 'react';
 import '../Styles/header.css';
 import cuh_logo from '../Images/cuh_logo.png';
 import { FiSearch } from 'react-icons/fi';
+import Logo from './Logo';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return ( 
         <div className="header">
             <div className="logo-text">
                 <img src={cuh_logo} alt="cuh_logo" />
-                <div className="text">
-                    <p>Reselling</p>
-                    <p>Platform</p>
-                </div>
+                <Logo/>
             </div>
             <div className="search-bar">
                 <input type="search" name="search" id="search" placeholder='Search items...'/>
                 <FiSearch className='icon'/>
             </div>
             <div className="login-sign">
-                <p>Login</p>
-                <p>Sign Up</p>
+                <p><Link to='/login'>Login</Link></p>
+                <p><Link to='/signup' style={{color:"white"}}>Sign Up</Link></p>
             </div>
         </div>
      );
